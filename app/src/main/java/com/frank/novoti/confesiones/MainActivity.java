d97a9d07821fd08afc79872ffb48cc6a7d12ca36
FRANK RINCON <rin16002@byui.edu>
2017-04-11 18:03:14 -0600

    Testing stuff.

--- app/src/main/java/com/frank/novoti/confesiones/MainActivity.java
+++ app/src/main/java/com/frank/novoti/confesiones/MainActivity.java
@@ -2,12 +2,21 @@ package com.frank.novoti.confesiones;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void postConfession() {
+        EditText postView = (EditText) findViewById(R.id.editText2);
+        String thePostText = (String) postView.getText().toString();
+
+        //Hola Renato
     }
 }
